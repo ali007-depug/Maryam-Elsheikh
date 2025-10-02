e0ef4d9eec08c06366ffcb8b8375c04750557a33
ali007-depug <aliroma849@gmail.com>
2025-10-02 22:14:23 +0200

    change Engineer hero img

--- src/data/Hero.ts
+++ src/data/Hero.ts
@@ -34,7 +34,7 @@ export const heroData: HeroData[] = [
     with 1+ years of training and courses. I trained as a 
     Quality Control Engineer at Sudanese Standard Metrology Organization 
     and Azal Pharma Company.`,
-    heroImg: "hero.webp",
+    heroImg: "hero2.webp",
     glowColor: "bg-gray-400",
     floatingIcons: [
       {
